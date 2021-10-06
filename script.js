4888a949dcee8684aed87acfce13f3cfed76cc26
harshadexhibyte <harshad05.exhibyte@gmail.com>
2021-10-06 16:12:49 +0530

    JS Fundamentals – Part 2,PROJECT #1 Guess My Number

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+'use strict';
+
 let js = 'amazing';
 
-if (js === 'amazing') alert('TEST')
+// if (js === 'amazing') alert('TEST')
 
 console.log(40 + 10 + 20 - 50 + 100)
 
@@ -134,6 +136,11 @@ function cutFruits(fruits) {
 }
 cutFruits(4)
 
+function MyName(name) {
+    console.log(`My Name is ${name},`);
+}
+MyName('Harshad');
+
 const FruitProccesor = function (oranges, apples) {
     const applesPieces = cutFruits(apples)
     const orangesPieces = cutFruits(oranges)
@@ -153,7 +160,148 @@ console.log(friends[1])
 console.log(friends[2])
 
 console.log(friends.length)
+
 friends.push('jay');
-console.log(friends)
-friends.pop()
-console.log(friends)
+friends.push('sahil');
+console.log(friends);
+
+friends.pop();
+console.log(friends);
+
+friends.unshift('ronak');
+console.log(friends);
+
+friends.shift();
+console.log(friends);
+
+console.log(friends.indexOf('keval'));
+console.log(friends.indexOf('jaymish'));
+console.log(friends.indexOf('ravi'));
+
+console.log(friends.includes('keval'));  //true
+console.log(friends.includes('harshad'));  //false
+
+const data = {
+    fname: 'satasiya',
+    lname: 'harshad',
+    email: 'harshad@gmail.com',
+    bod: '2000',
+    mobile: '7202848201',
+    friend: ['ravi', 'jaymish', 'keval'],
+    CalcBirth: function (birthYear) {
+        return 2021 - birthYear;
+    }
+
+};
+console.log(data.fname);
+console.log(data.lname);
+console.log(data.email);
+
+// const msg = prompt(`choose the friend 1 to ${friends.length}`);
+// console.log(friends[msg]);
+console.log(data.CalcBirth(2000));
+
+for (let rep = 1; rep <= 10; rep++) {
+    console.log(`Im repited ${rep} times`);
+}
+const types = [];
+const MyArry = [
+    'harshad',
+    'satasiya',
+    '2000',
+    true,
+    7202848201,
+    'harshad@gmail.com',
+]
+
+for (let i = 0; i < MyArry.length; i++) {
+    console.log(MyArry[i], typeof MyArry[i]);
+    types.push(typeof MyArry[i]);
+}
+console.log(types);
+
+for (let i = 0; i < MyArry.length; i++) {
+    console.log(MyArry[i], typeof MyArry[i]);
+    types.push([typeof MyArry[i], MyArry[i]]);
+}
+console.log(types);
+
+for (let i = MyArry.length; i >= 0; i--) {
+    console.log(i, MyArry[i]);
+}
+let repit = 1;
+while (repit <= 10) {
+    console.log(`While repit ${repit}`);
+    repit++;
+}
+
+while (10 <= repit) {
+    console.log(`While repit ${repit}`);
+    repit--;
+}
+
+let dice = Math.trunc(Math.random() * 6);
+console.log(dice);
+
+'use strict';
+
+const SecretNumber = Math.trunc(Math.random()*20+1);
+// document.querySelector('.number').textContent = SecretNumber;
+
+a = document.querySelector('.message').textContent;
+console.log(a);
+// document.querySelector('.message').textContent = 'Correct Number!';
+
+// document.querySelector('.number').textContent = 13;
+let score = document.querySelector('.score').textContent = 20;
+let highscore = 0;
+
+// document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+ 
+document.querySelector('.check').addEventListener('click',function () { 
+    console.log(document.querySelector('.guess').value);
+    const guess = Number(document.querySelector('.guess').value)
+
+    if(!guess){
+        document.querySelector('.message').textContent = 'No Number! Please Enter';   
+    }
+    else if(guess === SecretNumber ){
+        document.querySelector('.message').textContent = 'Correct Number!';
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        if(score > highscore){
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
+    }
+    else if(guess > SecretNumber ){
+        if(score > 0){
+            document.querySelector('.message').textContent = 'To high Number !';
+            score--;
+            document.querySelector('.score').textContent = score;
+        }
+        else{
+            document.querySelector('.message').textContent = 'You Lost The game!';
+        }
+    }
+    else if(guess < SecretNumber ){
+        if(score > 0){
+            document.querySelector('.message').textContent = 'To Low Number !';
+            score--;
+            document.querySelector('.score').textContent = score;
+        }
+        else{
+            document.querySelector('.message').textContent = 'You Lost The game!';
+        }
+    }
+});
+
+document.querySelector('.again').addEventListener('click',function(){
+    score = 20;
+    const  SecretNumber = Math.trunc(Math.random()*20+1);
+    document.querySelector('.message').textContent = 'Start guessing!';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = 0;
+
+    document.querySelector('body').style.backgroundColor = '#222';
+})
